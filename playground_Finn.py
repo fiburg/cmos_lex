ee9a999932881d385c41d258a57eb03938a59149
fiburg <finn.burgemeister@gmail.com>
2018-08-30 10:30:36 +0200

    - find_sun with algorithm, for other applications - ceilo init with utc info

--- playground_Finn.py
+++ playground_Finn.py
@@ -1,8 +1,15 @@
 import cmos
 import matplotlib.pyplot as plt
+import glob
+import os
 
 
 path = "/home/fibu/Studium/18_SoSe/Lehrexkursion/cmos_lex/data/alignment/"
 
 sky_imager_corr = cmos.SkyImagerSetup('hq')
-sky_imager_corr.find_sun(path)
+image_path_list = sorted(glob.glob(os.path.join(path, '*.jpg')))
+
+for img_path in image_path_list:
+    sun_position = sky_imager_corr.find_sun(path)
+    print(sun_position)
+
