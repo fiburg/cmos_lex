836c4352415f6e6e01420328b258f97813750f28
darklefknight <darklefknight@web.de>
2018-09-01 10:37:41 +0200

    - changing the live plot. - now map is matplotlib-api conform

--- cmos/live_plot.py
+++ cmos/live_plot.py
@@ -7,10 +7,15 @@ import time
 
 
 def live_plot(output_path, image_folder):
+
+    fig = plt.figure(figsize=(21,7))
     files = image_folder
     sky_imager = cmos.SkyImager("hq")
     map = cmos.Map()
-    map.make_map()
+
+    ax1 = plt.subplot(131)
+    ax2 = plt.subplot(132)
+    ax3 = map.make_map(subplot_info=133)
 
     while True:
         file = sorted(glob.glob(files))[-1]
@@ -25,36 +30,30 @@ def live_plot(output_path, image_folder):
         sky_imager.create_lat_lon_cloud_mask()
 
 
-        #
-        fig, (ax1,ax2,ax3) = plt.subplots(ncols=3)
-        #
-        # # Raw image:
-        # ax1.imshow(sky_imager.original_image)
-        #
-        # # Cloud Mask:
-        # ax2.imshow(sky_imager.cloud_image)
-        #
-        # # Cloud map:
+        # Raw image:
+        ax1.imshow(sky_imager.original_image)
 
 
-        map.set_positional_data(date=sky_imager.date,
+        # Cloud Mask:
+        ax2.imshow(sky_imager.cloud_image)
+
+
+        # Cloud map:
+        ax3.set_positional_data(date=sky_imager.date,
                                 cloud_height=sky_imager.cloud_height,
                                 sun_azimuth=sky_imager.sun_azimuth,
                                 sun_elevation=sky_imager.sun_elevation)
-        map.add_shadows(sky_imager.lat_lon_cloud_mask)
-        map.add_clouds(sky_imager.lat_lon_cloud_mask)
-        map.add_station_marker(sky_imager.instrument_name, sky_imager.lat, sky_imager.lon)
-        map.add_setting_title('CMOS - Clouds and shadows - HQ sky imager')
-        # map.save_plot(output_path)
-        map.remove_sky_values()
+        ax3.add_shadows(sky_imager.lat_lon_cloud_mask)
+        ax3.add_clouds(sky_imager.lat_lon_cloud_mask)
+        ax3.add_station_marker(sky_imager.instrument_name, sky_imager.lat, sky_imager.lon)
+        ax3.add_setting_title('CMOS - Clouds and shadows - HQ sky imager', size=15)
+        ax3.remove_sky_values()
 
-        # ax3 = map.ax
-
-        # plt.savefig(output_path)
+        plt.savefig(output_path)
 
         # plt.show()
         print("sleeping")
-        time.sleep(60)
+        time.sleep(10)
         print("sleeping done")
         # plt.close()
 
